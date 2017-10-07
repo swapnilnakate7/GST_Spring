cbb655ada2036766504de9ddbdd3971871ed6a44
swapnilnakate7 <nakate.swapnil7@gmail.com>
2017-10-07 16:08:55 +0530

    Initial Commit

---
+++ src/main/java/com/eduspaceinc/gst/GstSpringStarterApplication.java
@@ -0,0 +1,12 @@
+package com.eduspaceinc.gst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GstSpringStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GstSpringStarterApplication.class, args);
+	}
+}
